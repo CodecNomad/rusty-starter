4d670d1df9bfe891464d1e7ff71e2bc1e0c82e03
codec <codec@gmail.com>
2024-06-19 09:51:30 +0200

    - Fixed child process - Broke anything not windows - Multi threading

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::{
 
 use colored::Colorize;
 use dialoguer::FuzzySelect;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -27,7 +28,7 @@ fn main() {
         .expect("should have been able to parse profiles from config.toml");
     let profile_names: Vec<&str> = profiles
         .profiles
-        .iter()
+        .par_iter()
         .map(|p| p.profile_name.as_str())
         .collect();
 
@@ -37,13 +38,19 @@ fn main() {
         .interact()
         .expect("should have been able to interact with fuzzy selection");
 
-    profiles.profiles[selection].paths.iter().for_each(|path| {
-        if let Err(e) = Command::new(path).spawn() {
-            eprintln!("{} {}: {}", "[-]".red(), path, e);
-        } else {
-            println!("{} {}", "[+]".green(), path);
-        }
-    });
+    profiles.profiles[selection]
+        .paths
+        .par_iter()
+        .for_each(|path| {
+            if let Err(e) = Command::new("cmd.exe")
+                .args(["/c", &path.to_string()])
+                .spawn()
+            {
+                eprintln!("{} {}: {}", "[-]".red(), path, e);
+            } else {
+                println!("{} {}", "[+]".green(), path);
+            }
+        });
 
     println!("Auto closing in 5s..");
     sleep(Duration::from_secs(5));
